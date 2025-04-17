545e65bd0bf0baa217ede54893620472a9919a19
Pablo Rodriguez Defino <prodriguezdefino@gmail.com>
2025-04-17 13:38:55 -0700

    making the container build being triggered by changes on Dockerfiles

--- infra/artifact_registry.tf
+++ infra/artifact_registry.tf
@@ -6,10 +6,14 @@ resource "google_artifact_registry_repository" "repo" {
 }
 
 resource "null_resource" "build_and_push_beam_embeddings_image" {
+  triggers = {
+    docker_file = "${filesha1("../python-embeddings/Dockerfile")}"
+  }
+
   provisioner "local-exec" {
     command = <<EOF
       gcloud builds submit --tag ${var.region}-docker.pkg.dev/${var.project}/content-dicovery-platform-${var.run_name}/beam-embeddings:latest --project ${var.project} --region ${var.region}
-    
+
     EOF
     working_dir = "../python-embeddings"
   }
@@ -18,10 +22,14 @@ resource "null_resource" "build_and_push_beam_embeddings_image" {
 }
 
 resource "null_resource" "build_and_push_services_image" {
+  triggers = {
+    docker_file = "${filesha1("../services/Dockerfile")}"
+  }
+
   provisioner "local-exec" {
     command = <<EOF
       gcloud builds submit --tag ${var.region}-docker.pkg.dev/${var.project}/content-dicovery-platform-${var.run_name}/${var.run_name}-services:latest --project ${var.project} --region ${var.region}
-    
+
     EOF
     working_dir = "../services"
   }
